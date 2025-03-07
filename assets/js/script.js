6a32506f747a9333ca3ef9072b7c245c2e49d816
tumelo-maja <mogale.ramsey@gmail.com>
2025-03-07 17:11:46 +0000

    Add eventListeners to nut element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,19 @@
 // Add Event Listener to run only after DOM has loaded
 document.addEventListener("DOMContentLoaded", function() {
     console.log('Its loaded');
+    let nuts = document.querySelectorAll(".nut");
+    console.log(nuts);
+
+    for (let nut of nuts) {
+        nut.addEventListener('click', raiseNut);
+        }
+    
+    /**
+     * Raise the top nut when clicked
+     * and return to normal state
+     */
+    function raiseNut() {
+        console.log('Nut was clicked')
+    }
+    
 })
