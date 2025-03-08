4e39c568214a516a4cb4128f37fbb63ae6c31821
tumelo-maja <mogale.ramsey@gmail.com>
2025-03-08 19:39:43 +0000

    adjust code fix position issue with .raise-nut

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 // Add Event Listener to run only after DOM has loaded
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     console.log('Its loaded');
     let nuts = document.querySelectorAll(".nut");
     console.log(nuts);
 
     for (let nut of nuts) {
         nut.addEventListener('click', raiseNut);
-        }
-    
+    }
+
     /**
      * Raise the top nut when clicked
      * and return to normal state 
@@ -16,28 +16,20 @@ document.addEventListener("DOMContentLoaded", function() {
         let selectedNut = e.target;
         let currentNutWrap = selectedNut.parentElement;
         let currentRod = currentNutWrap.parentElement;
-        
+
         // console.log(selectedNut);
         // console.log(currentNutWrap);
         // console.log(currentRod);
         // only raise the top nut
         if (currentRod.lastElementChild === currentNutWrap) {
 
-            // selectedNut.classList.toggle("raise-nut");
+            selectedNut.classList.toggle("raise-nut");
             console.log("Definitely last born!");
-
-            currentNutWrap.style.position = 'absolute';
-            currentNutWrap.style.top = '-20px';
-            const wrapperRect = currentNutWrap.getBoundingClientRect();
-            console.log(wrapperRect);
-
-            const wrapperStyle = getComputedStyle(currentNutWrap);;
-            console.log(wrapperStyle.left);
 
         } else {
             console.log("Not the last child");
         }
 
     }
-    
+
 })
