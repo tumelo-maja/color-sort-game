bbe770b9a4ccad9c1e6efd88ed16797bffab881c
tumelo-maja <mogale.ramsey@gmail.com>
2025-03-08 17:43:12 +0000

    clear code to retain toggle for .raise-nut class

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,27 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
     function raiseNut(e) {
         let selectedNut = e.target;
          
-        //console.log(selectedNut);
-
-        // selectedNut.classList.toggle("raise-nut");
-
-        let nutStyle = getComputedStyle(selectedNut);
-        let nuts = document.querySelectorAll(".nut");
-
-        let nutPosition = this.getAttribute("data-number");
-
-        // offset = 30px (transform value) + marginBottom + height
-        let offset = 32 +(( parseInt(nutStyle.height)*2)*(8-nutPosition));
-        console.log(`offset: ${offset}`);
-        console.log(`marginBottom: ${parseInt(nutStyle.marginBottom)}`);
-        console.log(`height: ${parseInt(nutStyle.height)}`);
-        console.log(`Data number: ${nutPosition}`);
-        
-        selectedNut.style.transform = `translateY(-${offset}px)`; 
-        // selectedNut.style.backgroundColor = "red"; 
-        console.log(selectedNut);
-        // console.log(`margin: ${parseInt(nutStyle.marginBottom)}`);
-
+        selectedNut.classList.toggle("raise-nut");
 
     }
     
