9c02d2034eafad2961e1b968d6ac38375f74873c
tumelo-maja <mogale.ramsey@gmail.com>
2025-03-30 13:03:46 +0100

    Add vibration effect to win/lose sound howlers

--- assets/js/script.js
+++ assets/js/script.js
@@ -97,12 +97,18 @@ document.addEventListener("DOMContentLoaded", function () {
         gameWin: new Howl({
             src: ['assets/sounds/game-win-sfx.mp3',],
             volume: defaultVolume,
+            onplay: function () {
+                runVibration(100);
+            }
         }),
 
         completeRod: new Howl({
             src: ['assets/sounds/complete-rod3-sfx.mp3',],
             volume: defaultVolume,
             sprite: { rodWin: [1000, 1000] },
+            onplay: function () {
+                runVibration(100);
+            }
         }),
 
         collectPoints: new Howl({
