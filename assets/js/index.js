3cda4cf6e22541ba2617da6f6287442299bfe161
tumelo-maja <mogale.ramsey@gmail.com>
2025-03-31 15:59:01 +0100

    Add arrow icon to h3 elements and js scode

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,9 +3,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const playInstructionElement = document.querySelector('.play-instructions');
     const targetElement = document.querySelector('.game-instructions');
+    const arrowElement = playInstructionElement.querySelector(".bi-chevron-double-down");
 
     // Event listener to toggle hidden-item class on .game-instructions element 
     playInstructionElement.addEventListener('click', function () {
         targetElement.classList.toggle('hidden-item')
+        arrowElement.classList.toggle('rotate');
     });
 })
