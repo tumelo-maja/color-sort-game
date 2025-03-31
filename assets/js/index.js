762ca87916b19b25fc9520496b3400afffb85bee
tumelo-maja <mogale.ramsey@gmail.com>
2025-03-31 23:36:22 +0100

    Clear unused code

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,7 +5,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const targetElement = document.querySelector('.game-instructions');
     const arrowElement = playInstructionElement.querySelector(".bi-chevron-double-down");
 
-    // Event listener to toggle hidden-item class on .game-instructions element 
     playInstructionElement.addEventListener('click', function () {
         targetElement.classList.toggle('hidden-item')
         arrowElement.classList.toggle('rotate');
