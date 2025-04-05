d94102727b70049e0b45fb378b8c03a66c4fe2f2
tumelo-maja <mogale.ramsey@gmail.com>
2025-04-05 21:06:21 +0100

    Remove comments

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,16 +5,13 @@
  */
 document.addEventListener("DOMContentLoaded", function () {
 
-    // Declare the clickable h3 element 
     const playInstructionElement = document.querySelector('.play-instructions');
-    // Declare the game instruction div containing instructions to be displayed/hidden on toggle.
     const targetElement = document.querySelector('.game-instructions');
-    // Declare the arrow icon to be rotated 
     const arrowElement = playInstructionElement.querySelector(".bi-chevron-double-down");
 
     /**
-     * Add Event Listener to toggle the visibility of the gameplay instructions and
-     * rotate the arrow icon with each toggle.
+     * Add eventListener to toggle the visibility of the gameplay instructions
+     * and rotate the arrow icon with each toggle.
      */
     playInstructionElement.addEventListener('click', function () {
         targetElement.classList.toggle('hidden-item');
